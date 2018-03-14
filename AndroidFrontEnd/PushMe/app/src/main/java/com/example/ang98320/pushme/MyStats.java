03985b900476643fa1291ba03a41531eefba41a0
Alex Ng <ang98320@gmail.com>
2018-03-14 11:11:41 -0700

    Added Working files to repository

---
+++ AndroidFrontEnd/PushMe/app/src/main/java/com/example/ang98320/pushme/MyStats.java
@@ -0,0 +1,13 @@
+package com.example.ang98320.pushme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyStats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_stats);
+    }
+}
---
+++ AndroidFrontEnd/PushMe/app/src/main/java/com/example/ang98320/pushme/WheelSelection.java
@@ -0,0 +1,13 @@
+package com.example.ang98320.pushme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WheelSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wheel_selection);
+    }
+}
